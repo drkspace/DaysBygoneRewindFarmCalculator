d3670014aced4f881cbf68f50f15fb9a3edbf95b
Daniel Kramer <drkspace@gmail.com>
2021-11-16 14:08:37 -0700

    Added rounding and fixed formula

--- scripts.js
+++ scripts.js
@@ -16,26 +16,26 @@ function getRewindTime()
 
 function sellOff(P,G){
 
-  var so = (((1000-P)/G) % 1) %1;
+  var so = (1-((1000-P)/G) % 1) %1;
   so = so * G;
-  document.getElementById('selloff').innerHTML = "Sell Off amount:" +so;
+  document.getElementById('selloff').innerHTML = "Sell Off amount:" +so.toFixed(2);
   return so;
   }
 
   function sellOffRemainder(P,so){
 
-  document.getElementById('afterselloff').innerHTML = "Amount after selloff: " +(P-so);
+  document.getElementById('afterselloff').innerHTML = "Amount after selloff: " +(P-so).toFixed(2);
   }
 
   function rewindsLeft(P,G,a)
   {
-    rl = (1000-P+a)/G;
-    document.getElementById('rewindsLeft').innerHTML = "Rewinds Left: "+ rl;
+    rl = (1000-(P-a))/G;
+    document.getElementById('rewindsLeft').innerHTML = "Rewinds Left: "+ rl.toFixed(1);
     return rl;
   }
 
 function timeLeft(rl, T){
-  document.getElementById('timeLeft').innerHTML = "Time Left: "+ (rl*T/60)+" h";
+  document.getElementById('timeLeft').innerHTML = "Time Left: "+ (rl*T/60).toFixed(2)+" h";
 
 }
 
